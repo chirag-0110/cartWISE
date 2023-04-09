f52544f8263ad18112071a0cec9c8777ca43fe8b
chirag <chiragsharma1676@gmail.com>
2023-04-09 15:57:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter ,Route,Routes} from "react-router-dom";
+
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Reset from "./pages/auth/Reset";
+
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import Cart from "./pages/cart/Cart";
+import AddProduct from "./components/addproduct/AddProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <ToastContainer/>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/reset" element={<Reset/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/sellproduct" element={<AddProduct/>}/>
+        </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
